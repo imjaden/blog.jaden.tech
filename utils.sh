510e7c29cf5e41522f10e2b60123d17d0ead61bd
jaden.li <jaden.li@jaden.tech>
2022-02-27 11:40:07 +0800

    Modify: build automated

--- utils.sh
+++ utils.sh
@@ -7,7 +7,7 @@ install)
     ;;
 
 build)
-    prettier --write --ignore-unknown .
+    # prettier --write --ignore-unknown .
 
     rm -fr {_book,Online-Blog} 2 &>/dev/null
     gitbook build
@@ -20,6 +20,18 @@ build)
     prettier --write Online-Blog/index.html
     prettier --write Online-Blog/posts/*/*.html
     ;;
+
+build:automated)
+    bash ./utils.sh build
+
+    git add .
+    git commit -a -m "Modify: build automated"
+    git checkout posts
+
+    mv posts posts.deprecated
+    cp -rf Online-eBook/* .
+    ;;
+
 serve)
     # pm2 serve --port 4000 --no-daemon Online-Blog
     python3 -m http.server --directory Online-Blog 4000
